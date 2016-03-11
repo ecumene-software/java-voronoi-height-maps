86e9d5c4edacf2c465edf939e416f4822b63d287
ecumene-software <mitchell.hynes@ecumene.xyz>
2016-03-11 16:34:02 -0500

    Inital commit

---
+++ src/main/java/HeightMap.java
@@ -0,0 +1,5 @@
+/**
+ * Created by ecumene on 11/03/16.
+ */
+public class HeightMap {
+}
---
+++ src/main/java/Voronoi.java
@@ -0,0 +1,14 @@
+import ecumene.exo.sim.util.heightmap.channel.HeightChannel;
+import org.joml.Vector2f;
+
+public class Voronoi {
+    public VoronoiPoint[] points;
+
+    public Voronoi(VoronoiPoint ... points){
+        this.points = points;
+    }
+
+    public VoronoiPoint[] getPoints() {
+        return points;
+    }
+}
